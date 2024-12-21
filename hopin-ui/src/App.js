34c1722cc58954a22f108d8a872617152b2a3fa0
menitap <menitap@outlook.com>
2024-12-21 20:32:55 +0530

    defining routes

--- hopin-ui/src/App.js
+++ hopin-ui/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect  } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './LoginPage'; // Login Page Component
 
-function App() {
+const App = () => {
+const [rides, setRides] = useState([
+    { id: 1, departureTime: '10:30 AM', from: 'Location A', to: 'Location B', seatsLeft: 3, cost: '₹50' },
+    { id: 2, departureTime: '3:00 PM', from: 'Location C', to: 'Location D', seatsLeft: 2, cost: '₹60' },
+    { id: 3, departureTime: '5:30 PM', from: 'Location E', to: 'Location F', seatsLeft: 4, cost: '₹40' },
+    { id: 4, departureTime: '7:00 AM', from: 'Location G', to: 'Location H', seatsLeft: 1, cost: '₹70' },
+  ]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage rides={rides}/>} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
